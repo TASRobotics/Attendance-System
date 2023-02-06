6e35dee100891ca4017be9ca305e8a6da4fe8002
TheoreticalPotato <121378855+TheoreticalPotato@users.noreply.github.com>
2023-02-06 16:38:58 +0800

    Separated raid one and zero

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ sg.theme('DarkAmber')   #TODO: Change color theme
 layout = [  [sg.Text('Welcome To Raid One/Zero')],
             [sg.Text('TAS ID:'), sg.InputText()],
             [sg.Text('Name:'), sg.InputText()],
+            [sg.Text('Raid One Or Raid Zero'), sg.Combo(['Raid One', 'Raid Zero'])],
             [sg.Text('Subteam:'), sg.Combo(['Programming', 'Mechanical', 'Electrical', 'Design', 'Statistics', 'Logistics'])],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
@@ -25,23 +26,33 @@ while True:
         break
     
     # selects the right file for what month it is (makes it easier to check the attendance)
-    if currentDate.month == 1:
-        filename = "studentsJan.csv"
-    elif currentDate.month == 2:
-        filename = "studentsFeb.csv"
-    elif currentDate.month == 3:
-        filename = 'studentsMar.csv'
-    elif currentDate.month == 4:
-        filename = 'studentsApril.csv'
-    elif currentDate.month == 5:
-        filename = 'studentsMay.csv'
+    if currentDate.month == 1 and values[2] == 'Raid One':
+        filename = "oStudentsJan.csv"
+    elif currentDate.month == 1 and values[2] == 'Raid Zero':
+        filename = 'zStudentsJan.csv'
+    elif currentDate.month == 2 and values[2] == 'Raid One':
+        filename = "oStudentsFeb.csv"
+    elif currentDate.month == 2 and values[2] == 'Raid Zero':
+        filename = 'zStudentsFeb.csv'
+    elif currentDate.month == 3 and values[2] == 'Raid One':
+        filename = 'oStudentsMar.csv'
+    elif currentDate.month == 3 and values [2]== 'Raid Zero':
+        filename = 'zStudentsMar.csv'
+    elif currentDate.month == 4 and values [2] == 'Raid One':
+        filename = 'oStudentsApril.csv'
+    elif currentDate.month == 4 and values[2] == 'Raid Zero':
+        filename = 'zStudentsApril.csv'
+    elif currentDate.month == 5 and values[2] == 'Raid One':
+        filename = 'oStudentsMay.csv'
+    elif currentDate.month == 5 and values[2] == 'Raid Zero':
+        filename = 'zStudentsMay.csv'
 
     # the student values based on what they type
     students = [
-    {'id': values[0], 'name': values[1], 'subteam': values[2], 'timeAdded': currentDate}
+    {'id': values[0], 'name': values[1], 'team': values[2], 'subteam': values[3], 'timeAdded': currentDate}
 ]
     # The fields for the csv files
-    fields = ['id', 'name', 'subteam', 'timeAdded']
+    fields = ['id', 'name', 'subteam', 'team', 'timeAdded']
 
     # appends it onto the csv file
     with open(filename, 'a') as csvfile:
@@ -51,18 +62,17 @@ while True:
 
     # gives tasks based on what they choose
     #TODO: Give a task
-    if values[2] == 'Programming':
+    if values[3] == 'Programming' and values[2]:
         sg.popup('programming task')
-    elif values[2] == 'Mechanical':
+    elif values[3] == 'Mechanical':
         sg.popup('Mech task')
-    elif values[2] == 'Electrical':
+    elif values[3] == 'Electrical':
         sg.popup('electrical task')
-    elif values[2] == 'Design':
+    elif values[3] == 'Design':
         sg.popup('design task')
-    elif values[2] == 'Statistics':
+    elif values[3] == 'Statistics':
         sg.popup('Stats task')
-    elif values[2] == 'Logistics':
+    elif values[3] == 'Logistics':
         sg.popup('Logistics task')
 
 window.close()
-
